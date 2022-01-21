8d26c3606930094f02e9a2a49af98a17d908daf3
Mathbonc <mjbo@cin.ufpe.br>
2022-01-20 22:21:06 -0300

    Projeto 2

--- Learning_OpenCV/Learning_OpenCV/FaceDetection.cpp
+++ Learning_OpenCV/Learning_OpenCV/FaceDetection.cpp
@@ -16,11 +16,11 @@ int main() {
 	CascadeClassifier faceCascade;
 	faceCascade.load("Recursos/haarcascade_frontalface_default.xml");
 	if (faceCascade.empty()) {
-		cout << "Não Carregou!" << endl;
+		cout << "Nï¿½o Carregou!" << endl;
 	}
 
 	vector<Rect> faces;
-	faceCascade.detectMultiScale(img, faces, 1.1, 10); //Segundo número é o número mínimo de vizinhos
+	faceCascade.detectMultiScale(img, faces, 1.1, 10); //Segundo nï¿½mero ï¿½ o nï¿½mero mï¿½nimo de vizinhos
 
 	for (int i = 0; i < faces.size(); i++) {
 		rectangle(img, faces[i].tl(), faces[i].br(), Scalar(0, 0, 255), 2);
